7392baf1c666176a6a29be4f20e6cf5d92431532
ewansou <ewansou@hotmail.com>
2020-11-26 16:04:57 +0800

    Update HelloWorld.go

--- HelloWorld.go
+++ HelloWorld.go
@@ -6,4 +6,5 @@ func main() {
 	name := "Gabriel"
 	fmt.Printf("Hello, to the new world from, %s", name)
 	fmt.Println("hi gabriel. this is ewan. nice to meet you :)")
+	fmt.Println("this is ewan here. editing code in branch in github.com")
 }
