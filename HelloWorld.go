807c3def75d823d3770863a4dba29196b3e63b88
Gabriel <52558343+gabrielleeyj@users.noreply.github.com>
2020-11-27 13:15:10 +0800

    Delete HelloWorld.go

--- HelloWorld.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	name := "Gabriel"
-	fmt.Printf("Hello, to the new world from, %s", name)
-	fmt.Println("hi gabriel. this is ewan. nice to meet you :)")
-}
